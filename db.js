18463b9b376e2e992dc9490174d37e9a611088b9
vifi <vivitungkal8@gmail.com>
2026-05-24 11:28:43 +0700

    First commit: API Backup ExpressJS

---
+++ db.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql12/promise');
+let sql;
+const buatKoneksi = async () => {
+    return await mysql.createConnection({
+        host: 'localhost',
+        user: '?????',
+        password: '?????',
+        database: '?????'
+    });
+};
+const tambahBackup = async (id, nama, channel) => {
+    const db = await buatKoneksi();
+    sql = `INSERT INTO backup VALUES('${id}','${nama}','${channel}',NOW())`;
+    try{
+        await db.execute(sql);
+        return "1";
+    }catch(err){
+        return "0";
+    }
+};
+const tambahTransaksi = async (idx, id, waktux, nominalx, jenisx, deskripsix) => {
+    const db = await buatKoneksi();
+    sql = `INSERT INTO backup_transaksi VALUES('${idx}','${id}','${waktux}','${nominalx}','${jenisx}','${deskkripsix}')`;
+    try{
+       await db.execute(sql);
+       return "1"; 
+    }catch(err){
+        return "0";
+    }
+};
+module.exports = {buatKoneksi, tambahBackup, tambahTransaksi}
