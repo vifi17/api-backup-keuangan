c768c427eda70026f54c1f4adb0493f163aa0c1d
vifi <vivitungkal8@gmail.com>
2026-06-02 23:42:56 +0700

    p

--- api/index.js
+++ api/index.js
@@ -16,52 +16,52 @@
 //     });
 // });
 
-// app.post("/backup", async (req, res) => {
-//     try {
-//         let pesanx, kodex;
-//         const nama = req.body.nama_backup;
-//         const dtx = Buffer.from(req.body.dtx, 'base64').toString('utf-8');
-//         const id = Date.now();
-//         const arr_data = dtx.split("#");
-//         const proses = await db.tambahBackup(id, nama, "nodejs");
-//         if (proses == "1") {
-//             let berhasil = 0;
-//             let gagal = 0;
-//             for (const k of arr_data) {
-//                 if (!k) continue;
-//                 const arr_data2 = k.split("|");
-//                 const idx = arr_data2[0];
-//                 const deskripsix = arr_data2[1];
-//                 const waktux = arr_data2[2];
-//                 const nominalx = arr_data2[3];
-//                 const jenisx = arr_data2[4];
-//                 const proses2 = await db.tambahTransaksi( `${id}-${idx}`, id, waktux, nominalx, jenisx, deskripsix);
-//                 proses2 == "1" ? berhasil++ : gagal++;
-//             }
-//             pesanx = {
-//                 kode: "01",
-//                 status: "Proses Backup Berhasil",
-//                 berhasil: berhasil,
-//                 gagal: gagal
-//             };
-//             kodex = 200;
-//         } else {
-//             pesanx = {
-//                 kode: "00",
-//                 status: "Proses Backup Gagal"
-//             };
-//             kodex = 500;
-//         }
-//         return res.status(kodex).json(pesanx);
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             kode: "00",
-//             status: "Terjadi Error Server",
-//             error: error.message
-//         });
-//     }
-// });
+app.post("/backup", async (req, res) => {
+    try {
+        let pesanx, kodex;
+        const nama = req.body.nama_backup;
+        const dtx = Buffer.from(req.body.dtx, 'base64').toString('utf-8');
+        const id = Date.now();
+        const arr_data = dtx.split("#");
+        const proses = await db.tambahBackup(id, nama, "nodejs");
+        if (proses == "1") {
+            let berhasil = 0;
+            let gagal = 0;
+            for (const k of arr_data) {
+                if (!k) continue;
+                const arr_data2 = k.split("|");
+                const idx = arr_data2[0];
+                const deskripsix = arr_data2[1];
+                const waktux = arr_data2[2];
+                const nominalx = arr_data2[3];
+                const jenisx = arr_data2[4];
+                const proses2 = await db.tambahTransaksi( `${id}-${idx}`, id, waktux, nominalx, jenisx, deskripsix);
+                proses2 == "1" ? berhasil++ : gagal++;
+            }
+            pesanx = {
+                kode: "01",
+                status: "Proses Backup Berhasil",
+                berhasil: berhasil,
+                gagal: gagal
+            };
+            kodex = 200;
+        } else {
+            pesanx = {
+                kode: "00",
+                status: "Proses Backup Gagal"
+            };
+            kodex = 500;
+        }
+        return res.status(kodex).json(pesanx);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            kode: "00",
+            status: "Terjadi Error Server",
+            error: error.message
+        });
+    }
+});
 
 // module.exports = app;
 const express = require("express");
@@ -76,35 +76,4 @@ app.get("/", (req, res) => {
     res.send("API NORMAL");
 });
 
-app.post("/backup", (req, res) => {
-
-    let id = req.body.id;
-    let nama = req.body.nama;
-    let channel = req.body.channel;
-    let waktu = req.body.waktu;
-
-    db.query(
-        "INSERT INTO Backup(id, nama, channel, waktu) VALUES(?,?,?,?)",
-        [id, nama, channel, waktu],
-        (err, result) => {
-
-            if(err){
-
-                console.log(err);
-
-                return res.status(500).json({
-                    success: false,
-                    error: err.message
-                });
-
-            }
-
-            res.json({
-                success: true
-            });
-
-        }
-    );
-
-});
 module.exports = app;
