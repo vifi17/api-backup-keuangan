a2ef90454e1501fca1b42111c38c7deff3b87723
vifi <vivitungkal8@gmail.com>
2026-06-17 11:48:24 +0700

    k

--- api/index.js
+++ api/index.js
@@ -31,38 +31,24 @@ app.get("/", (req, res) => {
     res.send("API NORMAL");
 });
 app.post("/backup", async (req, res) => {
-
     try {
-
         let pesanx, kodex;
-
         let nama = req.body.nama_backup;
-
         let dtx = Buffer.from(req.body.dtx, 'base64').toString('utf-8');
-
         let id = Date.now();
-
         let arr_data = dtx.split("#");
-
         let proses = await db.tambahBackup(id, nama, "nodejs");
-
         if(proses == "1"){
-
             let berhasil = 0;
             let gagal = 0;
-
             for(let k of arr_data){
-
                 if (!k) continue;
-
                 let arr_data2 = k.split("|");
-
                 let idx = arr_data2[0];
                 let deskripsix = arr_data2[1];
                 let waktux = arr_data2[2];
                 let nominalx = arr_data2[3];
                 let jenisx = arr_data2[4];
-
                 let proses2 = await db.tambahTransaksi(
                     `${id}-${idx}`,
                     id,
@@ -71,45 +57,40 @@ app.post("/backup", async (req, res) => {
                     jenisx,
                     deskripsix
                 );
-
                 proses2 == "1"
                     ? berhasil++
                     : gagal++;
-
             }
-
             pesanx = {
                 kode: "01",
                 status: "Proses Backup Berhasil",
                 berhasil: berhasil,
                 gagal: gagal
             };
-
             kodex = 200;
-
         } else {
-
             pesanx = {
                 kode: "00",
                 status: "Proses Backup Gagal"
             };
-
             kodex = 500;
-
         }
-
         return res.status(kodex).json(pesanx);
-
     } catch(error){
-
         console.log(error);
-
         return res.status(500).json({
             error: error.message
         });
-
     }
+});
 
+app.get("/daftar_backup", async (req,res) => {
+    const dtbackup = await db.bacaBackup();
+    if(dtbackup == false){
+        res.send('{"kode":"00","pesan":"Data Backup Tidak Di Temukan"}');
+    }else{
+        res.send('{"kode":"01","pesan":"Data Backup Di Temukan","data":' + JSON.stringify(dtbackup) + '}');
+    }
 });
 
 module.exports = app;
